07397d8c7249646fe2bed6775ad5be3c66d13ca4
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2024-12-18 10:18:06 +0530

    Create L2.js

---
+++ Lectures/L2.js
@@ -0,0 +1,38 @@
+console.log("Hello, World!");
+// lecture 2 
+// Arithmetic operators
+// 1.ADD +
+// 2.Sub  - 
+// 3. Mul * 
+// 4. divide /
+// 5.exponent **
+// 6. Modulus %
+let students = 90;
+console.log(students-1);
+console.log(students*9);
+console.log(students**(0.5));
+console.log(students%9);
+console.log(students+1);
+
+// AUGUMENTED ASSIGNMENTS NUMBERS
+// students +=1 
+// students -= 1
+// students *= 1
+// studetns%=9 
+
+// increment and decrement 
+// students++
+// students--
+// ++students
+// --students
+
+
+// Operators precendence
+
+// 1.Parenthesis
+// 2.Exponents
+// 3. Dvide, Multiply oir Modulo;
+// 4. ADD OR sub
+
+let val = 12 % 5 + 8/2;
+console.log(val);
