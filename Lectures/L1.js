961befdd3b9e77a461ce569c528c79864d200db4
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2024-12-18 10:17:48 +0530

    Create L1.js

---
+++ Lectures/L1.js
@@ -0,0 +1,55 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+// 1.declaration 2.assignment or intialization 
+
+let x = 190;
+console.log(x);
+
+// let age = 19;
+// console.log(typeof age) 
+// type of the datatype
+// formatiing using template literal 
+// console.log(`hey thi is rajesh of age ${age} years old`);
+
+
+// STRINGS
+// to create a string use single or double qoutes
+
+let firstname = "rajesh";
+let lastname = ".k";
+console.log(typeof firstname);
+console.log(`hey ${firstname}${lastname} do you like pizze?`)
+
+// datatypes
+// 1. numbers or int
+// 2. decimal 
+// 3. boolean
+// 4. float
+
+// in js all the integers, float, decimals , long are treated as number 
+
+let integer = 9;
+let decimal = 9.0;
+let floatt = 9.0573;
+let bool = false;
+
+console.log(typeof integer)
+console.log(typeof decimal)
+console.log(typeof floatt)
+console.log(typeof bool)
+
+// dom manipulation of get the content text of the html tags
+
+// document.getElementById("id of the tag in that html doc").textContent = "rajesh" or `any other formatted stirng`;
+
+// typecasting
+
+let age = "789";
+age = Number(age);
+console.log(typeof age)
+
+// Bollean() String()
+// when the userinput is accepted it will be in the string datatype 
